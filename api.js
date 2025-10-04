64c9f4e6f20573e74db29936592da704058a30b5
daavidgj <davidgj201@gmail.com>
2025-10-04 00:42:01 -0300

    Atualização ReadMe + Melhorias

--- api.js
+++ api.js
@@ -10,12 +10,7 @@ import Morador from "./morador.js";
 
 /*
 Hierarquia ->
-Construtora> Condomínio> Bloco> Apartamento> Dono
-
-
-
-
-*/
+Construtora> Condomínio> Bloco> Apartamento> Dono*/
 dotenv.config();
 //Puxar Caminho Banco de Dados
 process.env.MONGO_URI;
@@ -87,9 +82,12 @@ app.get("/construtora/:id", async (req, res) => {
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
-
+//---Criar Construtora
 app.post("/construtora", async (req, res) => {
     try {
+        const { nome, telefone } = req.body;
+        if (!nome) return res.status(400).json({ erro: "Campo 'nome' é obrigatório" });
+        if (!telefone) return res.status(400).json({ erro: "Campo 'telefone' é obrigatório" });
         const novaConstrutora = await Construtora.create(req.body);
         res.status(201).json(novaConstrutora);
     } catch (error) {
@@ -97,8 +95,37 @@ app.post("/construtora", async (req, res) => {
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+//---Atualizar Construtora
+app.put("/construtora/:id", async (req, res) => {
+    try {
+        const construtoraAtualizada = await Construtora.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
 
+        res.status(200).json(construtoraAtualizada);
+    } catch (error) {
+        console.log("Erro ao atualizar construtora", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Deletar Construtora
+app.delete("/construtora/:id", async (req, res) => {
+    try {
+        const construtoraDeletada = await Construtora.findByIdAndDelete(req.params.id);
+        if (!construtoraDeletada) {
+            return res.status(404).json({ error: "Construtora não encontrada" });
+        }
+        res.status(200).json("Construtora deletada com sucesso");
+    } catch (error) {
+        console.log("Erro ao deletar construtora", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//
+//
+//
 //CRUD Condomínios
+//
 //---Listar Todos Condomínios
 
 app.get("/construtora/:id/condominio/all", async (req, res) => {
@@ -131,6 +158,9 @@ app.get("/condominio/:id", async (req, res) => {
 //---Criar Condomínio
 app.post("/construtora/:id/condominio", async (req, res) => {
     try {
+        const { nome, endereco } = req.body;
+        if (!nome) return res.status(400).json({ erro: "Campo 'nome' é obrigatório" });
+        if (!endereco) return res.status(400).json({ erro: "Campo 'endereco' é obrigatório" });
         const condominioCriado = await Condominio.create({
             nome: req.body.nome,
             endereco: req.body.endereco,
@@ -142,15 +172,51 @@ app.post("/construtora/:id/condominio", async (req, res) => {
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+//---Atualizar Condomínio
+app.put("/condominio/:id", async (req, res) => {
+    try {
+        const condominioAtualizado = await Condominio.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+
+        res.status(200).json(condominioAtualizado);
+    } catch (error) {
+        console.log("Erro ao atualizar construtora", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Deletar Condomínio
+app.delete("/condominio/:id", async (req, res) => {
+    try {
+        const condominioDeletado = await Condominio.findByIdAndDelete(req.params.id);
+        if (!condominioDeletado) {
+            return res.status(404).json({ error: "Condomínio não encontrado" });
+        }
+        res.status(200).json("Condomínio deletado com sucesso");
+    } catch (error) {
+        console.log("Erro ao deletar condomínio", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
 //CRUD Bloco
 //---Listar Blocos do Condomínio
 app.get("/condominio/:id/bloco/all", async (req, res) => {
     const blocos = await Bloco.find({ idCondominio: req.params.id });
     res.status(200).json(blocos);
 });
+//---Listar Bloco
+app.get("/condominio/bloco/:id", async (req, res) => {
+    const blocos = await Bloco.findOne({ idCondominio: req.params.id });
+    res.status(200).json(blocos);
+});
 //---Criar Bloco
 app.post("/condominio/:id/bloco", async (req, res) => {
     try {
+        const { nome, andares, elevadores, escada } = req.body;
+        if (!nome) return res.status(400).json({ erro: "Campo 'nome' é obrigatório" });
+        if (!andares) return res.status(400).json({ erro: "Campo 'andares' é obrigatório" });
+        if (!elevadores) return res.status(400).json({ erro: "Campo 'elevadores' é obrigatório" });
+        if (!escada) return res.status(400).json({ erro: "Campo 'escada' é obrigatório" });
         const blocoCriado = await Bloco.create({
             nome: req.body.nome,
             andares: req.body.andares,
@@ -158,37 +224,83 @@ app.post("/condominio/:id/bloco", async (req, res) => {
             escada: req.body.escada,
             idCondominio: req.params.id,
         });
+
         res.status(201).json(blocoCriado);
     } catch (error) {
         console.log("Erro ao criar bloco", error);
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+//---Atualizar Bloco
+app.put("/condominio/bloco/:id", async (req, res) => {
+    try {
+        const blocoAtualizado = await Bloco.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!blocoAtualizado) {
+            return res.status(404).json({ error: "Bloco nao encontrado" });
+        }
+
+        res.status(200).json(blocoAtualizado);
+    } catch (error) {
+        console.log("Erro ao atualizar Bloco", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Deletar Bloco
+app.delete("/condominio/bloco/:id", async (req, res) => {
+    try {
+        const blocoDeletado = await Bloco.findByIdAndDelete(req.params.id);
+        if (!blocoDeletado) {
+            return res.status(404).json({ error: "Bloco não encontrado" });
+        }
+        res.status(200).json("Bloco deletado com sucesso");
+    } catch (error) {
+        console.log("Erro ao deletar bloco", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
 //Crud Apartamentos
 //---Listar Apartamento
 app.get("/condominio/bloco/apartamento/:id", async (req, res) => {
-    const apartamento = await Apartamento.findOne(req.params.id);
-    //Corrigiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiir
-    const bloco = await Bloco.findOne({ _id: i.idBloco });
-    const condominio = await Condominio.findOne({ _id: bloco.idCondominio });
-    const responseApartamento = await apartamento(async (i) => {
-        const responseEndereco = {
-            endereco: condominio.endereco,
-            condominio: condominio.nome,
-            bloco: bloco.nome,
-            andar: i.andar,
+    try {
+        const apartamento = await Apartamento.findById(req.params.id);
+        if (!apartamento) {
+            return res.status(404).json({ error: "Apartamento não encontrado" });
+        }
+
+        const bloco = await Bloco.findById(apartamento.idBloco);
+        if (!bloco) {
+            return res.status(404).json({ error: "Bloco não encontrado" });
+        }
+
+        const condominio = await Condominio.findById(bloco.idCondominio);
+        if (!condominio) {
+            return res.status(404).json({ error: "Condomínio não encontrado" });
+        }
+
+        const responseApartamento = {
+            id: apartamento._id,
+            numero: apartamento.numero,
+            metragem: apartamento.metragem,
+            quartos: apartamento.quartos,
+            banheiros: apartamento.banheiros,
+            garagem: apartamento.garagem,
+            endereco: {
+                endereco: condominio.endereco,
+                condominio: condominio.nome,
+                bloco: bloco.nome,
+                andar: apartamento.andar,
+            },
         };
-        return {
-            numero: i.numero,
-            metragem: i.metragem,
-            quartos: i.quartos,
-            banheiros: i.banheiros,
-            garagem: i.garagem,
-            endereco: responseEndereco,
-        };
-    });
-    res.status(200).json(responseApartamento);
+
+        res.status(200).json(responseApartamento);
+    } catch (error) {
+        console.error("Erro ao buscar apartamento:", error);
+        res.status(500).json({ error: "Erro interno do servidor" });
+    }
 });
+
 //---Cadastrar Apartamento
 app.post("/condominio/bloco/:idBloco/apartamento", async (req, res) => {
     try {
@@ -214,7 +326,35 @@ app.post("/condominio/bloco/:idBloco/apartamento", async (req, res) => {
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+//---Atualizar Apartamento
+app.put("/condominio/bloco/apartamento/:id", async (req, res) => {
+    try {
+        const apartamentoAtualizado = await Apartamento.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!apartamentoAtualizado) {
+            return res.status(404).json({ error: "Apartamento não encontrado" });
+        }
 
+        res.status(200).json(apartamentoAtualizado);
+    } catch (error) {
+        console.log("Erro ao atualizar Bloco", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Deletar Apartamento
+app.delete("/condominio/bloco/apartamento/:id", async (req, res) => {
+    try {
+        const apartamentoDeletado = await Apartamento.findByIdAndDelete(req.params.id);
+        if (!apartamentoDeletado) {
+            return res.status(404).json({ error: "Apartamento não encontrado" });
+        }
+        res.status(200).json("Apartamento deletado com sucesso");
+    } catch (error) {
+        console.log("Erro ao deletar Apartamento", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
 //Crud Morador
 app.get("/morador/:cpf", async (req, res) => {
     const morador = await Morador.findOne({ cpf: req.params.cpf });
@@ -278,6 +418,34 @@ app.post("/condominio/bloco/apartamento/:idApartamento/morador", async (req, res
         }
     } catch (error) {
         console.log("Erro ao criar morador", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Atualizar Morador
+app.put("/morador/:cpf", async (req, res) => {
+    try {
+        const moradorAtualizado = await Morador.findOneAndUpdate({ cpf: req.params.cpf }, req.body, { new: true });
+
+        if (!moradorAtualizado) {
+            return res.status(404).json({ error: "Morador não encontrado" });
+        }
+
+        res.status(200).json(moradorAtualizado);
+    } catch (error) {
+        console.log("Erro ao atualizar morador", error);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+});
+//---Deletar Morador
+app.delete("/morador/:cpf", async (req, res) => {
+    try {
+        const moradorDeletado = await Morador.findOneAndDelete({ cpf: req.params.cpf });
+        if (!moradorDeletado) {
+            return res.status(404).json({ error: "Morador não encontrado" });
+        }
+        res.status(200).json("Morador deletado com sucesso");
+    } catch (error) {
+        console.log("Erro ao deletar morador", error);
         return res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
